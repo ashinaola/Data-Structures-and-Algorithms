b4fec02c78649c6ff023397ee6cd44b81ab22786
ashinaola <shinaolaagbede728@gmail.com>
2021-11-23 12:13:52 -0800

    Need to debug insertionSort() and modifiedInSort()

--- ArrayIns.java
+++ ArrayIns.java
@@ -12,6 +12,14 @@ class ArrayIns {
         nElems++;
     }
 
+    public long[] remove(long value) {
+        long[] copy = new long[array.length - 1];
+        for (int i = 0, j = 0; i < array.length; i++)
+            if (i != index)
+                copy[j++] = array[i];
+        return copy;
+    }
+
     public void display() {
         for(int i = 0; i < nElems; i++)
             System.out.print(a[i] + " ");
@@ -38,19 +46,34 @@ class ArrayIns {
 
     // removes duplicates from previously sorted array
     // make sure no item is moved more than once
-    public long[] noDups() {
-        long[] ret = new long[nElems];
-        
-        return ret;
+    public void noDups() {
+        for(int j = 0; j < nElems; j++) {
+            for(int k = j+1; j < nElems; k++) {
+                if(a[j] == a[k])
+                    remove(a[j]);
+            }
+        }
     }
 
     // adds a counter to count how many compares and swaps were done
+    // may need to adjust 
     public void modifiedInSort() {
-
+        int in, out;
+        int countSwap = 0;
+        int countComp = 0;
+        for(out = 1; out < nElems; out++) {
+            long temp = a[out];
+            in = out;
+            while(in > 0 && a[in-1] >= temp) {
+                a[in] = a[in-1];
+                --in;
+            }
+            a[in] = temp;
     }
 
     // sorts the array so that there are no duplicates (removes when duplicate is found)
     public void noDupSort() {
-
+        insertSort();
+        noDups();
     }
 }
